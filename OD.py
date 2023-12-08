93053f48e676aa3041b433923f4cc150bb27c7f9
Fouzul Hassan <72754118+fouzul-hassan@users.noreply.github.com>
2023-12-08 12:20:19 +0530

    prettify

--- OD.py
+++ OD.py
@@ -15,11 +15,8 @@ st.set_page_config(
     page_icon="👋",
 )
 
-
-st.title('OD Dashboard - AIESEC in Sri Lanka')
-
-
-@st.cache_data
+# Load data outside of Streamlit app initialization
+@st.cache_resource
 def load_data(data_url):
     try:
         data = pd.read_csv(data_url)
@@ -43,16 +40,9 @@ data = load_data(data_url1)
 data_url2 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4p6YJ0XKwY0AmS37dz_j7cuUG4uZYoZeFyCuWP0MBbjBgV7XXf2nqGompdTW-o-2x1CAxmIExoHXy/pub?gid=1230705189&single=true&output=csv'
 data_core = load_data(data_url2)
 
-# Load data
-data_load_state = st.text('Loading data...')
-data_load_state.text('Welcome to the OD Dashboard')
 
-# Function to filter data based on user selection
-@st.cache
-def filter_data(data, selected_entity, selected_month):
-    return data[(data['entity'] == selected_entity) & (data['month_name'] == selected_month)]
-
-
+# Set up Streamlit app title
+st.title('OD Dashboard - AIESEC in Sri Lanka')
 
 
 
@@ -71,6 +61,7 @@ selected_month = st.sidebar.selectbox('Select Month', month_list)
 filtered_data = data[(data['entity'] == selected_entity) & (data['month_name'] == selected_month)]
 filtered_data_entity = data[(data['entity'] == selected_entity)]
 filtered_data2 = filtered_data_month = data[(data['month_name'] == selected_month)]
+
 
 def plot_bubble_chart(filtered_data2):
     # Bubble plot using Plotly
@@ -145,8 +136,6 @@ def gen_bar_chart(selected_entity, selected_month, data):
         tooltip=['Function:N', 'Score:Q'],  # Include Function and Score in the tooltip
         # color=alt.value('blue')  # You can change the color if needed
     ).properties(
-        width=600,
-        height=400,
         title=f'Scores vs Function Name - {selected_entity} - {selected_month}'
     )
 
@@ -214,7 +203,6 @@ def display_kpi_metrics(selected_entity, selected_month, kpis, title):
                 , unsafe_allow_html=True)
 
      
-# Display XDI Scores
 xdi_kpis = ['DXP', 'iGTa', 'iGTe', 'iGV', 'oGTa', 'oGTe', 'oGV']
 display_kpi_metrics(selected_entity, selected_month, xdi_kpis, "XDI Scores")
 
@@ -226,6 +214,10 @@ display_kpi_metrics(selected_entity, selected_month, hdi_kpis, "HDI Scores")
 odi_kpis = ['ODI', 'XDI', 'HDI']
 display_kpi_metrics(selected_entity, selected_month, odi_kpis, "ODI Scores")
 
+"""
+
+
+"""
 # Generate bar chart
 gen_bar_chart(selected_entity, selected_month, data)
 
@@ -259,23 +251,34 @@ with col3:
 st.subheader(f'Entity vs Functions Scores for {selected_month}')
 filtered_data2 = data[data['month_name'] == selected_month]
 pivot_data = filtered_data2.set_index('entity').T.drop('month_name')
-st.write(pivot_data)
+# st.write(pivot_data)
+st.dataframe(pivot_data, use_container_width=True)
 
 # Plot bubble chart
 plot_bubble_chart(filtered_data2)
 
 # Add a dropdown for function selection in the sidebar
-function_list = ["FnL","BD","ER","TM","Brand","EM","IM","iGV","oGV","iGTa","iGTe","oGTa","oGTe","DXP"]
+function_list = ["FnL", "BD", "ER", "TM", "Brand", "EM", "IM", "iGV", "oGV", "iGTa", "iGTe", "oGTa", "oGTe", "DXP"]
 selected_function = st.selectbox('Select Function', function_list)
 
 # Display the relevant function data based on the selected function
-function_data = pivot_data.loc[[selected_function]]
+function_data = pivot_data.loc[[selected_function]].reset_index(drop=True)
+st.dataframe(function_data, use_container_width=True, hide_index=True)
 
-st.write(function_data)
 
-plot_score_bar_chart(filtered_data2, selected_function, 'green')
+# Create three columns for line charts
+col1, col2= st.columns(2)
 
-plot_score_line_chart(filtered_data_entity, selected_function, 'green')
+# Plot each chart in a separate column
+with col1:
+    plot_score_bar_chart(filtered_data2, selected_function, 'green')
+
+with col2:
+    plot_score_line_chart(filtered_data_entity, selected_function, 'green')
+
+# Create three columns for bar charts
+col1, col2 = st.columns(2)
+
 
 data_core_filtered = data_core[(data_core['Function'] == selected_function) & (data_core['month_name'] == selected_month)]
 columns_to_display = [col for col in data_core_filtered.columns if col not in ['month_name', 'Function']]
@@ -283,4 +286,4 @@ columns_to_display = [col for col in data_core_filtered.columns if col not in ['
 # Remove index column
 data_core_filtered_display = data_core_filtered[columns_to_display].reset_index(drop=True)
 
-st.write(data_core_filtered_display)
+st.dataframe(data_core_filtered_display, use_container_width=True)
